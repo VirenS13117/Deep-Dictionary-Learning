97b98aa1b597db64bff266ef6aaa0b9cad5f83c4
Virender Singh <virender13117@gmail.com>
2018-10-24 19:19:36 +0530

    removing files temporarily

--- Project/scripts/Wavelab850/Browsers/Contents.m
+++
@@ -1,7 +0,0 @@
-% Browsers:Contents v850 -- WaveLab Browsers
-%
-% This directory contains browsers for use with WaveLab
-%
-% One-D		-	One-D signal browser
-% WaveTour      -       Browser for Mallat's Book
-%
--- Project/scripts/Wavelab850/corfig10.m
+++
@@ -1,8 +0,0 @@
- 
- 
-%
-%  Part of Wavelab Version 850
-%  Built Tue Jan  3 13:20:38 EST 2006
-%  This is Copyrighted Material
-%  For Copying permissions see COPYING.m
-%  Comments? e-mail wavelab@stat.stanford.edu 
